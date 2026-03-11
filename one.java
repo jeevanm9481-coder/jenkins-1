38e0bfd57a6bc99450ae505984066c2fc45cd08e
ambika <ambika4x4@gmail.com>
2026-03-11 14:28:38 +0530

    change 1

---
+++ one.java
@@ -0,0 +1,24 @@
+class jenkins
+{ 
+	public static void main (string args[])
+	{
+		System.out.println("Hello welcome to jenkins");
+		System.out.println("India are champions !!!");
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
